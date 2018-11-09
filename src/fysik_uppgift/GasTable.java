f071b1b83298446be2620fc42625deebc12b1e9a
mtafar <mtanus_messi@hotmail.com>
2018-11-09 13:37:30 +0100

    Update src/fysik_uppgift/GasTable.java

--- src/fysik_uppgift/GasTable.java
+++ src/fysik_uppgift/GasTable.java
@@ -3,7 +3,10 @@ package fysik_uppgift;
 public enum GasTable {
 	AIR(1.29, 1.01), 
 	O2(1.43, 0.92), 
-	OXYGEN(1.43, 0.92)
+	OXYGEN(1.43, 0.92),
+	AR (1.78,0.52),
+	CL2(3.21,0.50),
+	
 	;
 
 	double density;
