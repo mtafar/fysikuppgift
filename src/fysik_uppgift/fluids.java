215d7222d3f2a3b782c5aec2146caa071f9ec1e3
mtafar <mtanus_messi@hotmail.com>
2018-11-09 13:31:09 +0100

    Update src/fysik_uppgift/fluids.java

--- src/fysik_uppgift/fluids.java
+++ src/fysik_uppgift/fluids.java
@@ -7,6 +7,7 @@ public enum fluids {
 	H2SO4(1.84, 1.38, 10.0, 336.0, 511.0),
 	CH3OH(0.791, 2.50, -98,65,1100),
 	C2H5OH(0.789,2.43, -177,78.2,841),
+	C6H6(0.879,1.71,6,80,393),
 	;
  	double density;
 	double heatCapacity;
