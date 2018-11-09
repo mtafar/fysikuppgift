2a29dc2f40c1884b1c75b946a67cf7456534a062
mtafar <mtanus_messi@hotmail.com>
2018-11-09 13:35:06 +0100

    Update src/fysik_uppgift/fluids.java

--- src/fysik_uppgift/fluids.java
+++ src/fysik_uppgift/fluids.java
@@ -8,6 +8,8 @@ public enum fluids {
 	CH3OH(0.791, 2.50, -98,65,1100),
 	C2H5OH(0.789,2.43, -177,78.2,841),
 	C6H6(0.879,1.71,6,80,393),
+	TERPENTIN(0.84,1.75,-10,180,293),
+	CCI4(1.596,0.84,-23,77,193)
 	;
  	double density;
 	double heatCapacity;
